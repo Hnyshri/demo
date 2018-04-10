a2ea76817d3ef851f775831bc6a85ecd1d6b4d4d
shriyansh <shriyanshgupta123@gmail.com>
2018-04-11 00:12:05 +0530

    Final part the project

--- main.py
+++ main.py
@@ -2,15 +2,71 @@
 from spy_detail import spy,Spy,ChatMessage
 from steganography.steganography import Steganography #library.class import module
 from datetime import datetime
+import csv
 
+date = datetime.now()
+print date
 print "Hello Detective"
 print 'Let\'s get started'
+
+def login_page():
+    username = raw_input("Enter your username: ")
+    password = raw_input("Enter your password: ")
+    checkpassword(username, password)
+def checkpassword(use, pwd):
+    if use == "admin" and pwd == "admin":
+        login(use)
+    else:
+        print "Your username and/or password was incorrect"
+        login_page()
+def login(use):
+    print "Welcome " + use
+    print "You have successfully logged in!"
+login_page()
+
 
 STATUS_MESSAGE = ['Galti bdi glti engineering','Attachment are good for email only ','Sleeping','Wake up']
 frnd1 = Spy("priya","Ms.",24,3.5)
 frnd2 = Spy("Raj","Mr.",26,4.5)
 friends = [frnd1,frnd2]
+message=[]
 
+def load_friends():
+    with open('friends.csv', 'rb') as friends_data:
+        reader = csv.reader(friends_data)
+        #next(reader)
+
+        for row in reader:
+            frnd = Spy(name=row[0],salutation=row[1],age=row[2],rating=row[3])
+            friends.append(frnd)
+
+load_friends()
+
+def load_chat():
+    with open('message.csv', 'rb') as message_data:
+        reader = csv.reader(message_data)
+       # next(reader)
+
+        for row in reader:
+            msg = ChatMessage(message=row[0],sent_by_me=row[1])
+            message.append(msg)
+        
+load_chat()
+
+def read_chat_history():
+    read_msg=select_frnd()
+
+    for chat in friends[read_msg].chats:
+        if chat.sent_by_me:
+            print('[%s]' % chat.time.strftime("%d %B %Y"))
+            print('%s' % 'you said : ')
+            print '%s' % chat.message
+        else:
+            print('[%s]' % chat.time.strftime("%d %B %Y"))
+            print('%s said : ' % friends[read_msg].name)
+            print '%s' % chat.message
+
+            
 def add_status(c_status):
     if c_status != None:
         print "Your current status is" + c_status
@@ -37,14 +93,20 @@ def add_friend():
     frnd =  Spy("","",0,0.0)
 
     frnd.name = raw_input("What is your name? ")
+    frnd.sal = raw_input("What should we call you? ")
     frnd.age = input("What is your age? ")
-    frnd.rating = input("What is your rating? ")
+    frnd.rating = input("What is your rating? ")    
+    frnd.is_online = True
     if len( frnd.name)>2 and 12<frnd.age<50 and frnd.rating>spy.rating:
-        friends.append(frnd)
-       
+        with open('friends.csv','a') as friends_data: # a is Opens a file for appending. The file pointer is at
+                                                    # the end of the file if the file exists. That is, the file
+                                                    #  is in the append mode. If the file does not exist, it
+                                                    #  creates a new file for writing.
+            writer = csv.writer(friends_data)
+            writer.writerow([frnd.name,frnd.sal,frnd.rating,frnd.age,frnd.is_online])    
     else:
         print " Friend can not be added "
-    return len(friends)
+    return len(friends) 
 
 
 def select_frnd():
@@ -83,7 +145,7 @@ def start_chat(spy_name,spy_age,spy_rating):
     current_status = None
     show_menu = True
     while show_menu:
-        choice = input("What do you want to do? \n 1. Add a status \n 2. Add a friend \n 3. Send a message \n 4. Read a message \n 0. Exit ")
+        choice = input("What do you want to do? \n 1. Add a status \n 2. Add a friend \n 3. Send a message \n 4. Read a message \n 5. Read chat History \n 0. Exit ")
         if choice==1:
             update_status_message = add_status(current_status)
             print "update status is : " + update_status_message 
@@ -96,8 +158,8 @@ def start_chat(spy_name,spy_age,spy_rating):
             send_message()
         elif choice==4:
             read_message()
-        elif choice==0:
-            show_menu = False
+        elif choice==5:
+            read_chat_history()
         else:
             print "invalid input "
  
